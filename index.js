42427b31a8f58c25fe07d095d6b365ebbf727f38
MichaelScarn1107 <120017608+MichaelScarn1107@users.noreply.github.com>
2023-01-25 12:15:22 +0000

    Generate Passwords function working properly

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordEl1 = document.getElementById("password-el-1")
-let passwordEl2 = document.getElementById("password-el-2")
-// let generatePassword = document.getElementById("generate-password-btn")
+const passwordEl1 = document.getElementById("password-el-1")
+const passwordEl2 = document.getElementById("password-el-2")
+const generateButton = document.getElementById("generate-password-btn")
 let passwordLength = 12
+
 
 function getRandomCharacter() {
     let randomCharacter = Math.floor(Math.random() * characters.length)
@@ -17,7 +18,9 @@ function generateRandomPassword() {
         randomPassword += getRandomCharacter()         
     }
     return randomPassword
-}
+  }
 
-passwordEl1.textContent = generateRandomPassword()
-passwordEl2.textContent = generateRandomPassword()
+  function getRandomPassword(){
+    passwordEl1.textContent = generateRandomPassword()
+    passwordEl2.textContent = generateRandomPassword()
+  }
