b77a20d983bc3eaea497b08219d38f73c43de62b
MichaelScarn1107 <120017608+MichaelScarn1107@users.noreply.github.com>
2023-01-26 10:01:53 +0000

    Reformatted with clear button and slider

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-console.log(characters)
-let passwordLength = 12
 
- function randomPassword() {
-    createPassword()
- }
+let password1El = document.getElementById("password1El")
+let password2El = document.getElementById("password2El")
+passwordLength = 12
 
- function createPassword() {
-    let password1El = document.getElementById("password-el-1")
-    let password2El = document.getElementById("password-el-2")
-    password1El.textContent = ''
-    password2El.textContent = ''
-    for (let i = 0; i < 12; i++) {
-        let randomPassword1 = Math.floor(Math.random() * characeters.length);
-        let randomPassword2 = Math.floor(Math.random() * characeters.length);
 
-        password1El.textContent += characters[randomPassword1]
-        password2El.textContent += characters[randompassword2]
-    }
- }
+function createRandomPassword() { 
+    password1El.textContent = ""
+    password2El.textContent = ""
+   for (let i = 0; i < output.innerHTML; i++) {
+       password1El.textContent += randomLetter() 
+       password2El.textContent += randomLetter()
+   } 
+}
+
+function randomLetter() { 
+     let randomElement = Math.floor(Math.random() * characters.length)
+    return characters[randomElement] 
+}
+
+function clearPassword() {
+    password1El.textContent = ""
+    password2El.textContent = ""
+}
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
